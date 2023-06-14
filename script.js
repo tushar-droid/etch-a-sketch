28c59d5d758da275b347f586b0464a40eed28e58
Tushar_mac <tusharsandhu3022@gmail.com>
2023-06-13 20:13:21 -0700

    Basic Functionality

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 window.onload = () =>{ 
+    makeGrid(16)
+}
+let prev_size = 0;
+function makeGrid(size){
+    prev_size=size;
     const gridContainer= document.querySelector(".grid");
-    for(let i=0;i<16;i++){
+    for(let i=0;i<size;i++){
         const rowDiv= document.createElement('div');
         rowDiv.className= `row${i}`;
-        rowDiv.style= 'display:flex; '
+        const ht= 960/size;
+        rowDiv.style= `display:flex;`
         gridContainer.appendChild(rowDiv);
-        for(let j=0;j<16;j++){
+        for(let j=0;j<size;j++){
             const colDiv= document.createElement('div');
             colDiv.className= `col${j}`;
             colDiv.classList.add('griditem');
+            colDiv.style= ` height:${ht}px; width:${ht}px; `;
             rowDiv.appendChild(colDiv);
             colDiv.addEventListener('mouseover', ()=>{
                 colDiv.classList.add('hovering');
@@ -18,3 +25,25 @@ window.onload = () =>{
     }
 }
 
+function removeGrid(){
+    for(let i=0;i<prev_size;i++){
+        const elem= document.querySelector(`.row${i}`);
+        elem.remove();
+    }
+}
+
+btn = document.querySelector('.prompt-button');
+btn.addEventListener("click", () =>{
+    let size=prev_size;
+    let condition = true;
+    size = parseInt(prompt("enter the size (less than 100)"));
+
+    while (size>100 || size<=0) {
+        alert("Please enter within bounds");
+        size= parseInt(prompt("enter the size (less than 100)"));
+    }
+
+
+    removeGrid();
+    makeGrid(size);
+})
